a90cfb8b4f2b197f5ad0029be5727936f4b7023b
Amit Kumar <amitsinghbadram@gmail.com>
2026-06-09 10:01:10 +0530

    url added

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import type { Client, MatchSuggestion, IMatcherNote } from '../types';
 
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000/api';
+const API_BASE_URL ='https://tdc-dqxo.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
